53fcdddc45c8d84a23aef5a2bbd239bfaa736d54
Malcolm Wigren <trekommafem2@gmail.com>
2019-04-09 21:20:45 +0200

    Reading of real sensor data works, but there is something wrong with the synchronization

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import scipy.interpolate as interpolate
 import sys
 from moviepy.video.io.bindings import PIL_to_npimage
@@ -9,7 +8,12 @@ import numpy as np
 import argparse
 import PIL
 from PIL import ImageFont, ImageDraw
+import subprocess
 import pdb
+import tempfile
+import os
+from datetime import datetime
+import json
 
 
 FONT_SIZE = 200
@@ -31,12 +35,14 @@ class SensorData:
     speed = None
     throttle = None
     steering = None
+    time_offset = None
 
 
 SENSOR_DATA = SensorData()
 
 
 def add_telemetry_data(get_frame, t):
+    offset = SENSOR_DATA.time_offset.seconds
     frame = get_frame(t)
     height, _, _, = frame.shape
     global FONT
@@ -47,15 +53,15 @@ def add_telemetry_data(get_frame, t):
         SMALL_FONT = ImageFont.FreeTypeFont("../fonts/PEPSI_pl.ttf", int(SMALL_FONT_SIZE*(height/1440)))
         TEXT_FRAME = get_static_text(frame.shape)
 
-    add_speed_text(frame, SENSOR_DATA.speed(t))
-    #add_throttle_bar(frame, SENSOR_DATA.throttle(t))
-    add_throttle_bar(frame, 1.0)
+    add_speed_text(frame, SENSOR_DATA.speed(t + offset))
+    add_throttle_bar(frame, SENSOR_DATA.throttle(t + offset))
+    #add_throttle_bar(frame, 1.0)
 
     overlay_image(frame, TEXT_FRAME, 0, 0)
-    plt.figure(1)
-    plt.imshow(frame)
-    plt.show()
-    sys.exit(0)
+    #plt.figure(1)
+    #plt.imshow(frame)
+    #plt.show()
+    #sys.exit(0)
     return frame
 
 
@@ -165,15 +171,68 @@ def overlay_image(frame, image, y, x):
     patch[:, :, :] = (1 - new_shit)*patch + new_shit*np_image
 
 
-def init_sensor_data(data_file_name):
-    data = None
-    with open(data_file_name) as f:
-        data = [tuple(float(c) for c in x.split(',')) for x in f.readlines()]
+def rpm_to_speed(rpm, ten_count_distance):
+    return 3.6*(rpm/60)*ten_count_distance
+
+
+def time_to_seconds(time):
+    time_split = time[1:].split(':')
+    hour = int(time_split[0])
+    minute = int(time_split[1])
+    second = float(time_split[2])
+    return hour*3600 + minute*60 + second
+
+
+def is_same_day(date1, date2):
+    return date1.year == date2.year and \
+            date1.month == date2.month and \
+            date1.day == date2.day
+
+
+def init_sensor_data(data_path, video_date, ten_count_distance):
+    csv_files = []
+    for root, _, files in os.walk(data_path):
+        csv_files.extend([(root, f) for f in files if f.endswith('.csv')])
+    
+    data = []
+    first_date = None
+    for root, csv_file in sorted(csv_files):
+        date = datetime.strptime("20" + csv_file, "%Y%m%d%H%M%S.csv")
+        if is_same_day(date, video_date):
+            if first_date is None:
+                first_date = date
+            lines = None
+            with open(os.path.join(root, csv_file)) as f:
+                lines = f.readlines()
+                #data = [tuple(float(c) for c in x.split(',')) for x in f.readlines()[6:]]
+
+            for line in lines[5:]:
+                line_split = line.split(',')
+                time = time_to_seconds(line_split[2])
+                steering = float(line_split[3])/100
+                throttle = float(line_split[4])/100
+                rpm = float(line_split[5])
+                speed = rpm_to_speed(int(rpm), ten_count_distance)
+
+                data.append((time, steering, throttle, speed))
 
     data_array = np.array(data)
-    SENSOR_DATA.speed = interpolate.interp1d(data_array[:, 0], data_array[:, 1])
+    SENSOR_DATA.speed = interpolate.interp1d(data_array[:, 0], data_array[:, 3])
     SENSOR_DATA.throttle = interpolate.interp1d(data_array[:, 0], data_array[:, 2])
-    SENSOR_DATA.steering = interpolate.interp1d(data_array[:, 0], data_array[:, 3])
+    SENSOR_DATA.steering = interpolate.interp1d(data_array[:, 0], data_array[:, 1])
+    SENSOR_DATA.time_offset = video_date - first_date
+    pdb.set_trace()
+
+
+def get_mp4_creation_date(mediafile):
+    output = subprocess.check_output(
+            ["./mp4info.sh", mediafile],
+            stderr=subprocess.STDOUT, timeout=3,
+            universal_newlines=True)
+    metadata = json.loads(output)
+    return datetime.strptime(metadata["streams"][0]["tags"]["creation_time"],
+                             "%Y-%m-%d %H:%M:%S")
+
 
 
 def main():
@@ -182,10 +241,15 @@ def main():
                        help='Path to the video file')
     parser.add_argument('-d', '--data', required=True, type=str,
                        help='Path to the telemetry data')
-
+    parser.add_argument('-t', '--ten-count-distance', type=float,
+                        help='Number of meters the vehicle travels every 10 revolutions',
+                        default=0.187)
+    
     args = parser.parse_args()
 
-    init_sensor_data(args.data)
+    video_date = get_mp4_creation_date(args.media)
+
+    init_sensor_data(args.data, video_date, args.ten_count_distance)
 
     videoclip = edit.VideoFileClip(args.media)
     videoclip = videoclip.fl(add_telemetry_data)
